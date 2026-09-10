cc622148ccb67bf226ae19cfbaf8610e7bf941ae
Muhammad Bilal Yousaf <bilalmughal1309@gmail.com>
2026-09-10 12:49:08 +0500

    CSRF token issue fixed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,14 +2,12 @@ import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideClientHydration(),
     provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
   ]
 };
